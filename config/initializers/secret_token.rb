298826201c60472a9f0c6655cb67c98d185facfd
mohanraj23 <smmohanraj23@gmail.com>
2013-07-16 18:59:23 +0530

    2nd initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quotethieves::Application.config.secret_token = 'd225a034b529234327d983b056866151bc03ccc3b61dcd40390c4ebb907ddf982b5140bf1f22482cb8f538b8dac2ad48840e020ad75c3b46ec0c06d2d30ff11e'
